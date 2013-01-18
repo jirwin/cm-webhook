d9bc2d95f71e533d3b7feece4a8f06572ecbab3c
Justin Gallardo <justin.gallardo@rackspace.com>
2013-01-18 15:48:42 -0800

    Remove extraneous things. Update deps in package.json.

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
+var express = require('express');
+var http = require('http');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var notify = require('./lib/notify').notify;
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 13044);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
-app.get('/settings', user.settings);
-app.post('/', routes.webhook);
+app.get('/', index);
+app.post('/', webhook);
+
+function index(req, res) {
+  res.send('cm-webhook');
+}
+
+function webhook(req, res) {
+  notify(req.body);
+  res.send('cm-webhook');
+}
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/user.js
+++
@@ -1,3 +0,0 @@
-exports.settings = function(req, res) {
-  res.render('users/settings', {});
-}
