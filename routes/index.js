ae341ebff5f368996bd2daa08aa40dde5d9c2af3
Justin Gallardo <justin.gallardo@rackspace.com>
2013-01-18 12:55:57 -0800

    Instead of sending a pre-made message to notifications, send the entire request body so each notification type can do what they want with it.

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-var sprintf = require('sprintf').sprintf;
 var notify = require('../lib/notify').notify;
 
 exports.index = function(req, res){
@@ -6,13 +5,7 @@ exports.index = function(req, res){
 };
 
 exports.webhook = function(req, res) {
-  var entityName = req.body.entity.label,
-      state = req.body.details.state,
-      status = req.body.details.status,
-      target = req.body.details.target,
-      msg;
-
-  msg = sprintf('%s(%s) is %s. %s', entityName, target, state, status);
-  notify(msg);
+  // Make notifications, passing the request body
+  notify(req.body);
   res.send();
 }
